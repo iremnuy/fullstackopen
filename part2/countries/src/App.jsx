e1855b66aead21f813fc7d65a2976ebf8aba3e63
İREM NUR YILDIRIM <irem.yildirim1@boun.edu.tr>
2025-09-02 18:16:24 +0300

    2.18 countries rest api connection

---
+++ part2/countries/src/App.jsx
@@ -0,0 +1,60 @@
+import { useState } from 'react'
+import CountrySummary from './components/CountrySummary'
+import CountryDetail from './components/CountryDetail'
+
+function App() {
+  //get the countires data from the restcountries api
+  const [countries, setCountries] = useState([])
+  const [newFilter, setNewFilter] = useState('')
+
+
+  const handleFilterChange = (event) => {
+    setNewFilter(event.target.value)
+    fetch(`https://restcountries.com/v3.1/name/${event.target.value}`)
+      .then(response => response.json())
+      .then(data => {
+        if (Array.isArray(data)) {
+          setCountries(data)
+        } else {
+          setCountries([])
+        }
+      }).
+      catch(error => {
+        console.error('Error fetching countries:', error)
+        setCountries([])
+      })
+  }
+
+  console.log("countries are" , countries)
+
+  const filteredCountries = countries.filter((country) =>
+  country.name.common.toLowerCase().includes(newFilter.toLowerCase())
+  )
+  
+
+  return (
+    <div>
+      <div>
+        find countries <input value={newFilter} onChange={handleFilterChange} /> 
+      </div>
+      <div>
+        {filteredCountries.length > 10 ? (
+          <p>Too many matches, specify another filter</p>
+        ) : filteredCountries.length === 1 ? (
+          <CountryDetail country={filteredCountries[0]} />
+        ) : (
+          filteredCountries.map((country) => (
+            <CountrySummary key={country.cca3} country={country} />
+          ))
+        )}
+      </div>
+    </div>
+  )
+
+  
+
+
+
+}
+
+export default App
---
+++ part2/countries/src/components/countrySummary.jsx
@@ -0,0 +1,10 @@
+
+const CountrySummary = ({ country }) => {
+    return (
+        <div>
+        {country.name.common} 
+        </div>
+    )
+    }
+
+export default CountrySummary;
