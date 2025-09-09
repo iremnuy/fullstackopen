721a5980d34a74569763e49c9a63a04f25e08a9d
İREM NUR YILDIRIM <irem.yildirim1@boun.edu.tr>
2025-09-09 13:50:33 +0300

    exercises up to 3.6

--- part3/index.js
+++ part3/index.js
@@ -36,3 +36,61 @@ app.get('/api/persons', (request, response) => {
     }
 )
 
+app.get('/info', (request, response) => {
+    const date = new Date()
+    response.send(`<p>Phonebook has info for ${notes.length} people</p><p>${date}</p>`)
+    }
+
+)
+app.get('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    const note = notes.find(note => note.id === id)
+    if (note) {
+        response.json(note)
+    } else {
+        response.status(404).end()
+    }
+    }
+)
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    const noteIndex = notes.findIndex(note => note.id === id)
+    if (noteIndex !== -1) {
+        notes.splice(noteIndex, 1) 
+        response.status(204).end()
+    } else {
+        response.status(404).end()
+    }
+    }
+)
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+
+    if (!body.name || !body.number) {
+        return response.status(400).json({ 
+            error: 'name or number is missing' 
+        })
+    }
+
+    const nameExists = notes.find(note => note.name === body.name)
+    if (nameExists) {
+        return response.status(400).json({ 
+            error: 'name must be unique' 
+        })
+    }
+
+    const newNote = {
+        id: (Math.random() * 10000).toFixed(0),
+        name: body.name,
+        number: body.number,
+    }
+
+    notes.push(newNote)
+    response.json(newNote)
+    }
+)
+
+
+
